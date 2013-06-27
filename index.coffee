d0e2525e9026923bc2c6ef8e9d647f9afef1968e
Andrey Popp <8mayday@gmail.com>
2013-06-27 00:25:02 -0700

    patch original methods, preserve cbs

--- index.coffee
+++ index.coffee
@@ -2,33 +2,37 @@ gitteh = require 'gitteh'
 {defer} = require 'kew'
 
 promisify = (func) ->
-  (args...) ->
-    promise = defer()
-    args.push (err, result) ->
-      if err then promise.reject(err) else promise.resolve(result)
-    func.call(this, args...)
-    promise
+  (args..., cb) ->
+    if typeof cb == 'function'
+      args.push(cb)
+      func.call(this, args...)
+    else
+      promise = defer()
+      args.push (err, result) ->
+        if err then promise.reject(err) else promise.resolve(result)
+      func.call(this, args...)
+      promise
 
-gitteh.promiseOpenRepository = promisify gitteh.openRepository
-gitteh.promiseInitRepository = promisify gitteh.initRepository
-gitteh.Blob::promiseSave = promisify gitteh.Blob::save
-gitteh.Commit::promiseSave = promisify gitteh.Commit::save
-gitteh.Commit::promiseTree = promisify gitteh.Commit::tree
-gitteh.Index::promiseAddEntry = promisify gitteh.Index::addEntry
-gitteh.Index::promiseFindEntry = promisify gitteh.Index::findEntry
-gitteh.Index::promiseGetEntry = promisify gitteh.Index::getEntry
-gitteh.Index::promiseWrite = promisify gitteh.Index::write
-gitteh.Reference::promiseDelete = promisify gitteh.Reference::delete
-gitteh.Reference::promiseResolve = promisify gitteh.Reference::resolve
-gitteh.Reference::promiseRename = promisify gitteh.Reference::rename
-gitteh.Reference::promiseSetTarget = promisify gitteh.Reference::setTarget
-gitteh.Repository::promiseReference = promisify gitteh.Repository::reference
-gitteh.Repository::promiseCommit = promisify gitteh.Repository::commit
-gitteh.Repository::promiseBlob = promisify gitteh.Repository::blob
-gitteh.Repository::promiseTree = promisify gitteh.Repository::tree
-gitteh.Repository::promiseCreateBlob = promisify gitteh.Repository::createBlob
-gitteh.Repository::promiseCreateCommit = promisify gitteh.Repository::createCommit
-gitteh.Repository::promiseCreateTag = promisify gitteh.Repository::createTag
-gitteh.Repository::promiseCreateTree = promisify gitteh.Repository::createTree
+gitteh.openRepository = promisify gitteh.openRepository
+gitteh.initRepository = promisify gitteh.initRepository
+gitteh.Blob::save = promisify gitteh.Blob::save
+gitteh.Commit::save = promisify gitteh.Commit::save
+gitteh.Commit::tree = promisify gitteh.Commit::tree
+gitteh.Index::addEntry = promisify gitteh.Index::addEntry
+gitteh.Index::findEntry = promisify gitteh.Index::findEntry
+gitteh.Index::getEntry = promisify gitteh.Index::getEntry
+gitteh.Index::write = promisify gitteh.Index::write
+gitteh.Reference::delete = promisify gitteh.Reference::delete
+gitteh.Reference::resolve = promisify gitteh.Reference::resolve
+gitteh.Reference::rename = promisify gitteh.Reference::rename
+gitteh.Reference::setTarget = promisify gitteh.Reference::setTarget
+gitteh.Repository::reference = promisify gitteh.Repository::reference
+gitteh.Repository::commit = promisify gitteh.Repository::commit
+gitteh.Repository::blob = promisify gitteh.Repository::blob
+gitteh.Repository::tree = promisify gitteh.Repository::tree
+gitteh.Repository::createBlob = promisify gitteh.Repository::createBlob
+gitteh.Repository::createCommit = promisify gitteh.Repository::createCommit
+gitteh.Repository::createTag = promisify gitteh.Repository::createTag
+gitteh.Repository::createTree = promisify gitteh.Repository::createTree
 
 module.exports = gitteh
--- index.js
+++ index.js
@@ -8,61 +8,66 @@ defer = require('kew').defer;
 
 promisify = function(func) {
   return function() {
-    var args, promise;
-    args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-    promise = defer();
-    args.push(function(err, result) {
-      if (err) {
-        return promise.reject(err);
-      } else {
-        return promise.resolve(result);
-      }
-    });
-    func.call.apply(func, [this].concat(__slice.call(args)));
-    return promise;
+    var args, cb, promise, _i;
+    args = 2 <= arguments.length ? __slice.call(arguments, 0, _i = arguments.length - 1) : (_i = 0, []), cb = arguments[_i++];
+    if (typeof cb === 'function') {
+      args.push(cb);
+      return func.call.apply(func, [this].concat(__slice.call(args)));
+    } else {
+      promise = defer();
+      args.push(function(err, result) {
+        if (err) {
+          return promise.reject(err);
+        } else {
+          return promise.resolve(result);
+        }
+      });
+      func.call.apply(func, [this].concat(__slice.call(args)));
+      return promise;
+    }
   };
 };
 
-gitteh.promiseOpenRepository = promisify(gitteh.openRepository);
+gitteh.openRepository = promisify(gitteh.openRepository);
 
-gitteh.promiseInitRepository = promisify(gitteh.initRepository);
+gitteh.initRepository = promisify(gitteh.initRepository);
 
-gitteh.Blob.prototype.promiseSave = promisify(gitteh.Blob.prototype.save);
+gitteh.Blob.prototype.save = promisify(gitteh.Blob.prototype.save);
 
-gitteh.Commit.prototype.promiseSave = promisify(gitteh.Commit.prototype.save);
+gitteh.Commit.prototype.save = promisify(gitteh.Commit.prototype.save);
 
-gitteh.Commit.prototype.promiseTree = promisify(gitteh.Commit.prototype.tree);
+gitteh.Commit.prototype.tree = promisify(gitteh.Commit.prototype.tree);
 
-gitteh.Index.prototype.promiseAddEntry = promisify(gitteh.Index.prototype.addEntry);
+gitteh.Index.prototype.addEntry = promisify(gitteh.Index.prototype.addEntry);
 
-gitteh.Index.prototype.promiseFindEntry = promisify(gitteh.Index.prototype.findEntry);
+gitteh.Index.prototype.findEntry = promisify(gitteh.Index.prototype.findEntry);
 
-gitteh.Index.prototype.promiseGetEntry = promisify(gitteh.Index.prototype.getEntry);
+gitteh.Index.prototype.getEntry = promisify(gitteh.Index.prototype.getEntry);
 
-gitteh.Index.prototype.promiseWrite = promisify(gitteh.Index.prototype.write);
+gitteh.Index.prototype.write = promisify(gitteh.Index.prototype.write);
 
-gitteh.Reference.prototype.promiseDelete = promisify(gitteh.Reference.prototype["delete"]);
+gitteh.Reference.prototype["delete"] = promisify(gitteh.Reference.prototype["delete"]);
 
-gitteh.Reference.prototype.promiseResolve = promisify(gitteh.Reference.prototype.resolve);
+gitteh.Reference.prototype.resolve = promisify(gitteh.Reference.prototype.resolve);
 
-gitteh.Reference.prototype.promiseRename = promisify(gitteh.Reference.prototype.rename);
+gitteh.Reference.prototype.rename = promisify(gitteh.Reference.prototype.rename);
 
-gitteh.Reference.prototype.promiseSetTarget = promisify(gitteh.Reference.prototype.setTarget);
+gitteh.Reference.prototype.setTarget = promisify(gitteh.Reference.prototype.setTarget);
 
-gitteh.Repository.prototype.promiseReference = promisify(gitteh.Repository.prototype.reference);
+gitteh.Repository.prototype.reference = promisify(gitteh.Repository.prototype.reference);
 
-gitteh.Repository.prototype.promiseCommit = promisify(gitteh.Repository.prototype.commit);
+gitteh.Repository.prototype.commit = promisify(gitteh.Repository.prototype.commit);
 
-gitteh.Repository.prototype.promiseBlob = promisify(gitteh.Repository.prototype.blob);
+gitteh.Repository.prototype.blob = promisify(gitteh.Repository.prototype.blob);
 
-gitteh.Repository.prototype.promiseTree = promisify(gitteh.Repository.prototype.tree);
+gitteh.Repository.prototype.tree = promisify(gitteh.Repository.prototype.tree);
 
-gitteh.Repository.prototype.promiseCreateBlob = promisify(gitteh.Repository.prototype.createBlob);
+gitteh.Repository.prototype.createBlob = promisify(gitteh.Repository.prototype.createBlob);
 
-gitteh.Repository.prototype.promiseCreateCommit = promisify(gitteh.Repository.prototype.createCommit);
+gitteh.Repository.prototype.createCommit = promisify(gitteh.Repository.prototype.createCommit);
 
-gitteh.Repository.prototype.promiseCreateTag = promisify(gitteh.Repository.prototype.createTag);
+gitteh.Repository.prototype.createTag = promisify(gitteh.Repository.prototype.createTag);
 
-gitteh.Repository.prototype.promiseCreateTree = promisify(gitteh.Repository.prototype.createTree);
+gitteh.Repository.prototype.createTree = promisify(gitteh.Repository.prototype.createTree);
 
 module.exports = gitteh;
